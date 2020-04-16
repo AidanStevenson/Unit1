bf92a9eb5c2f8844ad6636df063733d1b8270848
AidanStevenson <815103@mystma.org>
2020-04-16 15:53:53 +0000

    while loops assignment

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,10 +4,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         int count = 0;
 
-        while (count < 10) {
+        while (count <= 10) {
             System.out.println("The count is " + count);
-        }
+            count = count + 1;
 
+            System.out.println("count finished");
+
+     while (color == "red"){
+         System.out.println("The color is " + color);
+         color = "green";
+
+     }
+
+        System.out.println("color finished");
+
+        while (count <= 20)
+        System.out.println("the count is " + count);
+        count = count + 5;
         System.out.println("count finished");
-    }
-}
