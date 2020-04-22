d071c0f5238e8c8d7c425c0be142f8baa06e4636
AidanStevenson <815103@mystma.org>
2020-04-22 15:53:47 +0000

    Logic challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,15 +2,22 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        System.out.println("Start first Loop");
-        for (int i = 0; i <= 19; i = i +1){
-        System.out.println("End first loop");
+    
+    System.out.println("Start first loop");
 
-        System.out.println("Start second loop");
-        for (int i = -1; i <= 19; i = i +1){
-        System.out.println("End second loop");
+    for (int i = 20; i < 0; i-1){
+        System.out.println(i);
+        if (i == 10){
+    
+    System.out.println("End first loop");
 
-        }
-    }
+    System.out.println("Start second loop");
+    for (int a = 4; a < 36; a++){
+        System.out.println(a);
+        if (a == 12)
+    }   System.out.println("Hey!");
+
+    System.out.println("End second loop");
+    
+    }    
 }
