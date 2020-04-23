69381f222b58899eb8380694e2636bf6cca56eef
AidanStevenson <815103@mystma.org>
2020-04-23 16:29:32 +0000

    Double or single digit

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,21 +3,27 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
     
-    System.out.println("Start first loop");
+        System.out.println("Double or single digit");
+        for (int i = 20; i > 0; i = i - 1){
+            System.out.println(i);
+            if (i > 10){
+            System.out.println("double digit"); 
+                for (int j = 1; < 20; j = j - 1){
+                    System.out.println(i);
+                    if (i < 10){
+                    System.out.println("Single digit");
+                        for (int q = 9; > 1: q = q - 1){
+                        System.out.println(i);
+                        System.out.println("Hey!");    
+                            for (int w = 0; w < 5; w++){
+                            System.out.println("Nice!");
+                            }
 
-    for (int i = 20; i < 0; i-1){
-        System.out.println(i);
-        if (i == 10){
-    
-    System.out.println("End first loop");
-
-    System.out.println("Start second loop");
-    for (int a = 4; a < 36; a++){
-        System.out.println(a);
-        if (a == 12)
-    }   System.out.println("Hey!");
-
-    System.out.println("End second loop");
-    
-    }    
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+    
