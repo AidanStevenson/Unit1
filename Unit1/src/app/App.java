35f4f41b4f4d31486a3a4be3975f57f05f532418
AidanStevenson <815103@mystma.org>
2020-04-28 17:36:37 +0000

    input and output challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,21 +1,24 @@
 package app;
+import java.util.Scanner;
 
 public class App{
     public static void main(String[] args) throws Exception{
     
-        System.out.println("Start first loop");
-        for (int i = 1; i < 11; i++){
-            System.out.println(i);
-            if (i == 5){
-                System.out.println("Five");
-            }
-        }
-        System.out.println("End first loop");
-        System.out.println("Start second loop");
-        for (int i = 100; i < 210; i = i + 10){
-            System.out.println(i);
-        }
-        System.out.println("End second loop");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter you first name");
+        String firstName = input.nextLine();
+        System.out.println("Please enter your last name");
+        String lastName = input.nextLine();
+        System.out.println("Please enter you age");
+        String age = input.nextLine();
+        System.out.println("Please enter your favorite color");
+        String color = input.nextLine();
+
+        System.out.println("Your first name is " + firstName);
+        System.out.println("Your Last name is " + lastName);
+        System.out.println("Your age is " + age);
+        System.out.println("Your favorite colors are " + color);
     }   
 }
         
