b7c6331e1f887dc15b38f98bd019f036a3691e92
AidanStevenson <815103@mystma.org>
2020-05-06 16:58:16 +0000

    MID LIB 2

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,29 +6,47 @@ public class App{
     
         Scanner input = new Scanner(System.in);
 
-        System.out.println("how many hours are in a day?");
-        String hoursString = input.nextLine();{
-            if (hoursString.equals("24"))
-            System.out.println("correct");
-            }
-            
-        
-        System.out.println("How many states are in the united states?");
-        String statesString = input.nextLine();{
-            if (statesString.equals("50"))
-            System.out.println("correct");
-            }
+        System.out.println("Please enter a noun");
+        String nounString = input.nextLine();
 
-        
-        System.out.println("How many classes does each student have?");
-        String classesString = input.nextLine();{
-            if (classesString.equals("5"))
-            System.out.println("correct");
-            }
+        System.out.println("Please enter a name");
+        String nameString = input.nextLine();
 
-         
-       
-         
+        System.out.println("Please enter a verb");
+        String verbString = input.nextLine();
+
+        System.out.println("Please enter an adjective");
+        String adjectiveString = input.nextLine();
+
+        System.out.println("Please enter a place");
+        String placeString = input.nextLine();
+
+        System.out.println("Please enter a food");
+        String foodString = input.nextLine();
+
+        System.out.println("Please enter an animal");
+        String animalString = input.nextLine();
+
+        System.out.println("Please enter a number");
+        String numberString = input.nextLine();
+
+        System.out.println("You entered:");
+
+        System.out.println("Noun: " + nounString);
+
+        System.out.println("Name: " + nameString);
+
+        System.out.println("Verb: " + verbString);
+
+        System.out.println("Adjective: " + adjectiveString);
+
+        System.out.println("Place: " + placeString);
+
+        System.out.println("Food: " + foodString);
+
+        System.out.println("Animal: " + animalString);
+
+        System.out.println("Number: " + numberString);   
     }   
 }
         
